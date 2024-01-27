0b2790b1590cbe6e022ae762fd49033d0ed94f9a
Ryusei <sidapple@gmail.com>
2024-01-27 22:14:29 +0900

    first commit

---
+++ src/main/java/com/example/kankokey/KankoKeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.kankokey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KankoKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KankoKeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kankokey/KankoKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kankokey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KankoKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
